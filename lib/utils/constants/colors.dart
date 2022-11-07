78b33b1d85e2065338098ded07d6b598824029c7
thejorabek <boyxorozovjorabek@gmail.com>
2022-11-07 16:42:55 +0500

    Colors initial

---
+++ lib/utils/constants/colors.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+
+class MyColors {
+  static Color yellow = const Color(0xFFFFCD41);
+  static Color green = const Color(0xFFC9FFCB);
+  static Color blue = const Color(0xFFD4F8FB);
+  static Color pink = const Color(0xFFF8E5FF);
+}
--- lib/views/splash_screen.dart
+++ lib/views/splash_screen.dart
@@ -6,6 +6,8 @@ class SplashScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      
+    );
   }
 }
